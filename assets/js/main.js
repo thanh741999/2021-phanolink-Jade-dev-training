5795c0afad3693be8da9e3e492356ddc8f901b2e
thanh <vtthanh741999@gmail.com>
2021-07-04 13:54:30 +0700

    feat(index.html): complete home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,4 +118,47 @@ $(document).ready(function () {
             // instead of a settings object
         ]
     });
+    // ======== trademark carousel======
+    $('.trademark__content').slick({
+        infinite: true,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        prevArrow: '.trademark__prev',
+        nextArrow: '.trademark__next',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                infinite: true,
+                prevArrow: false,
+                nextArrow: false
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                prevArrow: false,
+                nextArrow: false
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                prevArrow: false,
+                nextArrow: false
+            }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 })
